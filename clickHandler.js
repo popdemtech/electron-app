eb04f02d0df5a3af8e9e6c7115d9e31562d32463
popdemtech <popdemtech@gmail.com>
2022-04-03 20:06:32 -0500

    Walkthrough branch

--- clickHandler.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('click', () => {
-	alert('Hello, Electron!');
-});
--- preload.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-  for (const dependency of ['chrome', 'node', 'electron']) {
-    let element = document.getElementById(`${dependency}-version`);
-    element.innerText = process.versions[dependency];
-  }
-});
